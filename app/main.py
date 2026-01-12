92f91346027351a3482c19c72d7889c324bd1a23
wonkwh <wonkwh@nc-and.com>
2026-01-12 18:11:40 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
+from imaplib import Commands
 import sys
 
 
 def main():
     sys.stdout.write("$ ")
+    command = input()
+    if command == "help":
+        print("Available commands:")
+        print("help - display this help message")
+        print("quit - exit the shell")
+    elif command == "quit":
+        sys.exit(0)
+    else:
+        print(f"{command}: command not found")
     pass
 
 

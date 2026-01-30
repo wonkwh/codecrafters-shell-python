2cdbdf7febfbbecbe5802bf9e7481cffd49d9d64
wonkwh <wonkwh@nc-and.com>
2026-01-30 14:32:28 +0900

    Locate executable files

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 
 logging.basicConfig(
@@ -6,6 +7,24 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
 )
 logger = logging.getLogger(__name__)  # 모듈명 기반 로거
+
+
+def find_executable(cmd: str) -> str | None:
+    # Check if the command is a built-in command
+    # if cmd in ["echo", "exit", "type"]:
+    #     return cmd
+    path_value = os.environ.get("PATH")
+    if not path_value:
+        return None
+
+    for directory in path_value.split(os.pathsep):
+        full_path = os.path.join(directory, cmd)
+
+        # 실행권한 확인
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+
+    return None
 
 
 def main():
@@ -27,7 +46,11 @@ def main():
             logger.debug(f"Type command received: {sub_cmd}")
             # buildin[sub_cmd] 가 없을 경우
             if sub_cmd not in builtin:
-                print(f"{sub_cmd}: not found")
+                path = find_executable(sub_cmd)
+                if path is None:
+                    print(f"{sub_cmd}: not found")
+                else:
+                    print(f"{sub_cmd} is {path}")
             else:
                 builtin[sub_cmd]()
         elif command.__contains__("echo"):
